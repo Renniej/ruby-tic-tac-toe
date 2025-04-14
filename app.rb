24d2126f0c706b6c6b626ab6ace6c55e2f5d615c
Rennie <7017622+Renniej@users.noreply.github.com>
2025-04-13 23:43:31 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+# Business logic
+# Enter player 1's name
+# Enter player 2's name
+#
+# While the game is not a tie or has a winner
+#   ask the next player where they would like to place their symbol
+#     if the that cell is already full, inform the player and ask them to choose again.
+#   after the move check for these win conditions
+#       left diagonal
+#       right diagonal
+#       horizontal
+#       vertical
+#   if a win or tie condition exist then exit the game
+#   output winner or if it was a tie
+#
+
+#
+#
+#
+#
